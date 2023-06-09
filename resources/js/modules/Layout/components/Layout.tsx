bb95d6d8f583b9d8eab86245b76b3df900bad936
Color-Kat <katik7979@gmail.com>
2023-06-09 19:44:54 +0300

    burger menu

--- resources/js/modules/Layout/components/Layout.tsx
+++ resources/js/modules/Layout/components/Layout.tsx
@@ -18,10 +18,11 @@ export const Layout: React.FC<{ children: React.ReactElement }> = memo(({childre
             className="scroll-container flex h-screen overflow-auto overflow-x-hidden font-circle bg-gray-200"
         >
 
-            <header className="flex justify-center lg:items-center lg:w-[300px] w-full h-max bg-white lg:px-0 px-4">
+            <header className="relative flex justify-center lg:items-center lg:w-[300px] w-full h-max lg:px-0 px-4 bg-white">
 
                 <nav className="flex lg:flex-col lg:pt-8 lg:pb-0 pt-3 pb-3 justify-between items-center lg:h-screen w-full">
 
+                    {/* Open mobile menu button */}
                     <button
                         onClick={() => setIsMobileMenuOpen(prev => !prev)}
                         className="absolute xs:relative lg:hidden text-3xl p-2 border bg-app text-app-accent h-max"
@@ -31,6 +32,7 @@ export const Layout: React.FC<{ children: React.ReactElement }> = memo(({childre
                             : <HiOutlineMenu/>
                     }</button>
 
+                    {/* Logo block */}
                     <div className="flex flex-col items-center lg:pb-8 lg:border-b lg:ml-0 xs:ml-[13%] mx-auto xs:mx-0 border-gray-200">
                         <img
                             src={logo}
@@ -43,6 +45,7 @@ export const Layout: React.FC<{ children: React.ReactElement }> = memo(({childre
                         </p>
                     </div>
 
+                    {/* Phone block */}
                     <div className="hidden xs:flex items-center flex-col">
                         <div className="mb-3 flex gap-1.5 items-center">
                             <img className="h-4" src={phone} alt="phone"/>
@@ -56,10 +59,12 @@ export const Layout: React.FC<{ children: React.ReactElement }> = memo(({childre
                         </RedButton>
                     </div>
 
+                    {/* Navigation */}
                     <div className="hidden lg:flex ">
 
                     </div>
 
+                    {/* footer block */}
                     <div className="hidden lg:flex bg-app px-[50px] pb-5 pt-10 text-[#3b4256] flex-col">
                         <a href="" className="mb-9 uppercase text-xs">Инфраструктура</a>
 
@@ -76,6 +81,16 @@ export const Layout: React.FC<{ children: React.ReactElement }> = memo(({childre
                         </div>
                     </div>
                 </nav>
+
+                {/*  Mobile menu  */}
+                <nav className={`lg:hidden flex ${isMobileMenuOpen ? 'h-64' : 'h-0'} w-full absolute bottom-0 translate-y-full bg-white  transition-all overflow-hidden z-10 px-4`}>
+                    213
+                </nav>
+
+                {/* Mobile menu overlay */}
+                {isMobileMenuOpen &&
+                    <div className="w-screen h-screen absolute top-0 left-0" onClick={() => setIsMobileMenuOpen(false)}/>
+                }
             </header>
 
             {/*<Header/>*/}
