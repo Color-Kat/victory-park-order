b364eb646c17d25bcfdd1536e1f0036b27ec00ab
Color-Kat <katik7979@gmail.com>
2023-06-11 22:43:25 +0300

    gallery module

---
+++ resources/js/modules/Gallery/components/Gallery.tsx
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+
+import {Navigation, Pagination, Scrollbar, A11y, FreeMode} from 'swiper';
+import {Swiper, SwiperRef, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import leftArrow from "@assets/icons/l-arrow.png";
+import rightArrow from "@assets/icons/r-arrow.png";
+
+interface GalleryProps {
+    photos: string[];
+}
+
+export const Gallery: React.FC<GalleryProps> = ({photos}) => {
+    const navigationPrevRef = React.useRef(null);
+    const navigationNextRef = React.useRef(null);
+
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(1);
+    const slidesCount = photos.length;
+
+    return (
+        <>
+            <Swiper
+                navigation={{
+                    prevEl: navigationPrevRef.current,
+                    nextEl: navigationNextRef.current,
+                }}
+                onBeforeInit={(swiper) => {
+                    // @ts-ignore
+                    swiper.params.navigation.prevEl = navigationPrevRef.current;
+                    // @ts-ignore
+                    swiper.params.navigation.nextEl = navigationNextRef.current;
+                }}
+                onRealIndexChange={(swiper) => {
+                    setCurrentSlideIndex(swiper.realIndex+1);
+                }}
+                loop={true}
+                modules={[Navigation, Pagination]}
+                slidesPerView={1}
+                className="w-full h-full absolute top-0 left-0 z-[-1]"
+            >
+                {photos.map(photo => (
+                    <SwiperSlide
+                        key={photo}
+                        className=""
+                    >
+                        <img
+                            src={photo}
+                            alt=""
+                            className="w-full h-full object-cover"
+                        />
+                    </SwiperSlide>
+                ))}
+
+                {/*{Array.from(Array(10).keys()).map(i => (*/}
+                {/*    <SwiperSlide*/}
+                {/*        key={i}*/}
+                {/*        className=""*/}
+                {/*    >*/}
+                {/*        <img*/}
+                {/*            src={`/storage/gallery/${i}.jpg`}*/}
+                {/*            alt=""*/}
+                {/*            className="w-full h-full object-cover"*/}
+                {/*        />*/}
+                {/*    </SwiperSlide>*/}
+                {/*))}*/}
+            </Swiper>
+
+            <div className="absolute left-0 bottom-10 w-full justify-center flex items-center gap-8">
+                <div ref={navigationPrevRef} className="cursor-pointer">
+                    <img src={leftArrow} alt=""/>
+                </div>
+
+                <div className="">
+                    {currentSlideIndex}
+                    <span className="mx-2">/</span>
+                    {slidesCount}
+                </div>
+
+                <div ref={navigationNextRef} className="cursor-pointer">
+                    <img src={rightArrow} alt=""/>
+                </div>
+            </div>
+        </>
+    );
+}
--- resources/js/pages/HomePage/components/GallerySection.tsx
+++ resources/js/pages/HomePage/components/GallerySection.tsx
@@ -1,20 +1,7 @@
-import React, {useCallback, useRef, useState} from 'react';
-import firstScreenImg from "@assets/images/first-screen.jpg";
-
-import {Navigation, Pagination, Scrollbar, A11y, FreeMode} from 'swiper';
-import {Swiper, SwiperRef, SwiperSlide} from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-
-import leftArrow from "@assets/icons/l-arrow.png";
-import rightArrow from "@assets/icons/r-arrow.png";
+import React from 'react';
+import {Gallery} from "@modules/Gallery";
 
 export const GallerySection: React.FC = ({}) => {
-    const navigationPrevRef = React.useRef(null);
-    const navigationNextRef = React.useRef(null);
-
-    const [currentSlideIndex, setCurrentSlideIndex] = useState(1);
-    const [slidesCount, setSlidesCount] = useState(3);
 
     return (
         <div
@@ -28,56 +15,8 @@ export const GallerySection: React.FC = ({}) => {
                 </h2>
             </div>
 
-            {/*<div>*/}
-            <Swiper
-                navigation={{
-                    prevEl: navigationPrevRef.current,
-                    nextEl: navigationNextRef.current,
-                }}
-                onBeforeInit={(swiper) => {
-                    // @ts-ignore
-                    swiper.params.navigation.prevEl = navigationPrevRef.current;
-                    // @ts-ignore
-                    swiper.params.navigation.nextEl = navigationNextRef.current;
-                }}
-                onRealIndexChange={(swiper) => {
-                    setCurrentSlideIndex(swiper.realIndex+1);
-                }}
-                loop={true}
-                modules={[Navigation, Pagination]}
-                slidesPerView={1}
-                // scrollbar={{draggable: true}}
-                className="w-full h-full absolute top-0 left-0 z-[-1]"
-            >
-                {Array.from(Array(10).keys()).map(i => (
-                    <SwiperSlide
-                        key={i}
-                        className=""
-                    >
-                        <img
-                            src={`/storage/gallery/${i}.jpg`}
-                            alt=""
-                            className="w-full h-full object-cover"
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            <Gallery photos={Array.from(Array(10).keys()).map(i => `/storage/gallery/2/${i}.jpg`)}/>
 
-            <div className="absolute left-0 bottom-10 w-full justify-center flex items-center gap-8">
-                <div ref={navigationPrevRef} className="cursor-pointer">
-                    <img src={leftArrow} alt=""/>
-                </div>
-
-                <div className="">
-                    {currentSlideIndex}
-                    <span className="mx-2">/</span>
-                    {slidesCount}
-                </div>
-
-                <div ref={navigationNextRef} className="cursor-pointer">
-                    <img src={rightArrow} alt=""/>
-                </div>
-            </div>
         </div>
     );
 }
