bd4dad7bfdf91b37d5c0b024407a617215c40bdc
Color-Kat <katik7979@gmail.com>
2023-06-11 17:14:29 +0300

    call request modal

--- resources/js/components/Modals/CallRequestModal.tsx
+++ resources/js/components/Modals/CallRequestModal.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useTDispatch, useTSelector} from "@hooks/redux.ts";
 
 import {closeModal} from "@/store/modal/modal.slice.tsx";
+import Input from "@UI/Form/Input.tsx";
+import {RedButton} from "@UI/Buttons";
 
 interface ModalProps {
 
@@ -12,16 +14,37 @@ export const CallRequestModal: React.FC<ModalProps> = ({}) => {
     const {isOpen} = useTSelector(state => state.modalCallRequest);
     const dispatch = useTDispatch();
 
+    const [form, setForm] = useState({
+        name: '',
+        phone: ''
+    });
+
     return (
         <div
-            className={`${isOpen ? 'opacity-1 translate-y-0' : 'opacity-0 -translate-y-16 pointer-events-none'} fixed w-screen h-screen top-0 left-0 text-3xl text-red-500 z-30 transition`}
-            onClick={() => dispatch(closeModal())}
+            className={`${isOpen ? 'opacity-1 translate-y-0 shadow-2xl' : 'opacity-0 -translate-y-8 pointer-events-none'} fixed w-screen h-screen top-0 left-0 bg-black/[.7] flex items-center justify-center z-50 transition duration-300`}
         >
-            <div className="modal">
-                <span className="modal-close">
+            <div
+                className="absolute top-0 left-0 w-full -m-16 h-full z-0"
+                onClick={() => dispatch(closeModal())}
+            />
+
+            <div className="relative modal px-16 py-16 bg-white z-10">
+                <div
+                    className="absolute top-4 right-4 cursor-pointer font-bold text-gray-600 text-2xl"
+                    onClick={() => dispatch(closeModal())}
+                >
                     &#10005; {/* HTML code for a multiplication sign */}
-                </span>
-                This is my new modal!
+                </div>
+
+                <div className="flex flex-col justify-center gap-5">
+                    <h3 className="text-app-accent font-bold text-2xl text-center">Заказать звонок</h3>
+
+                    <Input name="name" placeholder="Имя" value={form.name} setForm={setForm} type="text"/>
+
+                    <Input name="phone" placeholder="Телефон" value={form.phone} setForm={setForm} type="text"/>
+
+                    <RedButton filled={true} className="sm:w-full">Отправить</RedButton>
+                </div>
             </div>
         </div>
     );
