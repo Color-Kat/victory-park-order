d30ff9717f38fe7298aec96ac9add4ec663495e2
Color-Kat <katik7979@gmail.com>
2023-06-29 19:42:54 +0300

    fix monarch

--- resources/js/modules/Layout/components/Main.tsx
+++ resources/js/modules/Layout/components/Main.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {Footer} from "@modules/Layout";
+import {useLocation} from "react-router-dom";
 
 const Main: React.FC<{ children: React.ReactElement }> =
     ({children}) => {
+        const { pathname } = useLocation();
+        const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+        // Scroll to top
+        useEffect(() => {
+            scrollContainerRef.current?.scrollTo(0, 0);
+        }, [pathname]);
+
         return (
             <>
                 <main
                     className="flex-auto text-white scroll-container overflow-y-auto overflow-x-hidden"
+                    ref={scrollContainerRef}
                 >
                     {children}
 
--- resources/js/pages/InfrastructurePage/InfrastructurePage.tsx
+++ resources/js/pages/InfrastructurePage/InfrastructurePage.tsx
@@ -4,10 +4,10 @@ import {Link} from "react-router-dom";
 import illustration from "@assets/images/first-screen.jpg";
 import {TextSection} from "@UI/Sections/TextSection.tsx";
 
-import partner_logo_1 from "@assets/partner-logos/1.jpg";
-import partner_logo_2 from "@assets/partner-logos/2.jpg";
-import partner_logo_3 from "@assets/partner-logos/3.jpg";
-import partner_logo_4 from "@assets/partner-logos/4.jpg";
+const partner_logo_1 = "/storage/partner-logos/1.jpg";
+const partner_logo_2 = "/storage/partner-logos/2.jpg";
+const partner_logo_3 = "/storage/partner-logos/3.jpg";
+const partner_logo_4 = "/storage/partner-logos/4.jpg";
 
 import "./infrastructure.scss";
 import {Helmet} from "react-helmet";
@@ -66,7 +66,7 @@ export const InfrastructurePage: React.FC = () => {
                     Арендаторы бизнес-центра {(import.meta as any).env.VITE_BC_NAME}
                 </h2>
 
-                <div className="partner-logos flex items-center sm:my-16 my-8 gap-3 justify-between flex-wrap max-w-5xl mx-auto">
+                <div className="partner-logos flex items-center sm:my-16 my-8 gap-3 justify-between flex-wrap max-w-5xl mx-auto sm:flex-row flex-col">
                     <img src={partner_logo_1} alt="Mr. Doors"/>
                     <img src={partner_logo_2} alt="Miele"/>
                     <img src={partner_logo_3} alt="СТС media"/>
