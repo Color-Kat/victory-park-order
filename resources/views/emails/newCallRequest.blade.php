87039c5dd9e88488b0effee2022ae453973b2a74
Color-Kat <katik7979@gmail.com>
2023-07-12 19:43:00 +0300

    riversidetowers captcha

--- resources/views/emails/newCallRequest.blade.php
+++ resources/views/emails/newCallRequest.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Новая заявка Victory Park</title>
+    <title>{{$data['title']}}</title>
 </head>
 <body>
-<h1>Новая заявка Victory Park (<a href="http://minskayaplaza.ru/">Перейти на сайт</a>)</h1>
+<h1>{{$data['title']}} (<a href={{$data['site']}}>Перейти на сайт</a>)</h1>
 <h2>Имя: {{$data['name']}}</h2>
 <p>Тел.: <a href="tel:{{$data['phone']}}">{{$data['phone']}}</a></p>
 
