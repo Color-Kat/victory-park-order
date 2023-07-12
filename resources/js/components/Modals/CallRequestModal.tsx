5c26faf5767377121769b160c580cb66063cf056
Color-Kat <katik7979@gmail.com>
2023-07-12 21:59:35 +0300

    monarchbc captcha+emails

--- resources/js/components/Modals/CallRequestModal.tsx
+++ resources/js/components/Modals/CallRequestModal.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {useTDispatch, useTSelector} from "@hooks/redux.ts";
 
 import {closeCallRequestModal} from "@/store/modals/modals.slice.tsx";
@@ -8,22 +8,27 @@ import {useRequestCallMutation} from "@/store/offices/offices.api.ts";
 import Checkbox from "@UI/Form/Checkbox.tsx";
 import {isPhoneNumber} from "@/utils/formValidation.ts";
 import {BsTelephone} from "react-icons/bs";
+import ReCaptcha from "react-google-recaptcha";
+import {Loader} from "@UI/Loaders/Loader.tsx";
 
 interface ModalProps {
 
 }
 
 export const CallRequestModal: React.FC<ModalProps> = ({}) => {
-
     const {isCallRequestModalOpen, callRequestModalData} = useTSelector(state => state.modals);
     const dispatch = useTDispatch();
     const [requestCall] = useRequestCallMutation();
 
+    const reCaptchaRef = useRef<ReCaptcha>();
+
     const [isSent, setIsSent] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [form, setForm] = useState({
         name: '',
         phone: '',
-        agree: false
+        agree_1: false,
+        agree_2: false,
     });
 
     const [error, setError] = useState<boolean|string>(false);
@@ -36,14 +41,27 @@ export const CallRequestModal: React.FC<ModalProps> = ({}) => {
     const sendRequest = async () => {
         if(!form.name || !form.phone) return showError("Пожалуйста, заполните все поля.", "all");
         if(!isPhoneNumber(form.phone)) return showError("Введите корректный номер телефона", "phone");
-        if(!form.agree) return showError("Вы должны принять политику обработки персональных данных", "agree");
+        if (!form.agree_1) return showError('Вы должны принять пользовательское соглашение', 'agree_1');
+        if (!form.agree_2) return showError('Вы должны дать согласие на обработку персональных данных', 'agree_2');
         else showError(false);
 
-        await requestCall({
+        setIsLoading(true);
+
+        const captchaToken = await reCaptchaRef.current?.executeAsync();
+        const result = await requestCall({
             name: form.name,
             phone: form.phone,
-            ...callRequestModalData
+            'g-recaptcha-response': captchaToken ?? '',
+            ...callRequestModalData,
         });
+
+        reCaptchaRef.current?.reset(); // reset recaptcha
+
+        setIsLoading(false);
+
+        // Some errors
+        if('data' in result && result.data && 'error' in result.data || 'error' in result)
+            return showError((result as any)?.data?.error ?? 'Произошла какая-то ошибка');
 
         setIsSent(true);
     }
@@ -72,6 +90,12 @@ export const CallRequestModal: React.FC<ModalProps> = ({}) => {
                         {error}
                     </div>
 
+                    <ReCaptcha
+                        sitekey={(import.meta as any).env.VITE_CAPTCHA_SITE_KEY}
+                        size='invisible'
+                        ref={reCaptchaRef as any}
+                    />
+
                     <Input
                         name="name"
                         placeholder="Имя"
@@ -92,19 +116,35 @@ export const CallRequestModal: React.FC<ModalProps> = ({}) => {
                         error={errorField == "phone" || (errorField == "all" && form.phone == "")}
                     />
 
-                    <Checkbox
-                        name="agree"
-                        checked={form.agree}
-                        setForm={setForm}
-                    >
-                        Отправляя свои данные, я соглашаюсь с <a href="/personal-data" className="underline" target="_blank">Политикой обработки персональных данных</a> и <a href="/terms-of-service" className="underline" target="_blank">Пользовательским соглашением</a>
-                    </Checkbox>
+                    <div className="flex flex-col">
+                        <Checkbox
+                            name="agree_1"
+                            checked={form.agree_1}
+                            setForm={setForm}
+                        >
+                            Отправляя свои данные, я соглашаюсь с <a href="/terms-of-service" className="underline" target="_blank">Пользовательским соглашением</a> и <a href="/privacy-policy" className="underline" target="_blank">Политикой конфиденциальности</a>
+                        </Checkbox>
+
+                        <Checkbox
+                            name="agree_2"
+                            checked={form.agree_2}
+                            setForm={setForm}
+                        >
+                            Даю согласие на <a href="/personal-data" className="underline" target="_blank">Обработку персональных данных</a>
+                        </Checkbox>
+                    </div>
 
                     <RedButton 
                         filled={true} 
                         className="sm:w-full"
                         onClick={sendRequest}
-                    >Отправить</RedButton>
+                    >
+                        {
+                            isLoading
+                                ? <><Loader />Загрузка</>
+                                : <>Отправить</>
+                        }
+                    </RedButton>
                 </div>}
 
                 {isSent && <div className="mt-4 text-lg text-center max-w-xs">
--- resources/views/emails/newCallRequest.blade.php
+++ resources/views/emails/newCallRequest.blade.php
@@ -5,23 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Новая заявка Victory Park</title>
+    <title>{{$data['title']}}</title>
 </head>
 <body>
-<h1>Новая заявка Victory Park (<a href="http://minskayaplaza.ru/">Перейти на сайт</a>)</h1>
+<h1>{{$data['title']}} (<a href={{$data['site']}}>Перейти на сайт</a>)</h1>
 <h2>Имя: {{$data['name']}}</h2>
 <p>Тел.: <a href="tel:{{$data['phone']}}">{{$data['phone']}}</a></p>
 
-@if($data['email'])
+@if(isset($data['email']))
     <p>E-mail: <a href="mailto:{{$data['email']}}">{{$data['email']}}</a></p>
 @endif
 
 <p>
-    @if($data['message'])
+    @if(isset($data['message']))
         Сообщение: <br> {{$data['message']}} <br/><br/>
     @endif
 
-    @if($data['officeCrmId'])
+    @if(isset($data['officeCrmId']))
         crmId офиса: {{$data['officeCrmId']}} <br/>
         Площадь офиса: {{$data['officeSpace']}} <br/>
     @endif
